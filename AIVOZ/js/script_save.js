db3d604919bcad6fe4a3d17f5ac39792fbcc1ee7
Lucas Marques Bandeira <69401421+lucasmarques2020@users.noreply.github.com>
2021-08-26 23:41:11 -0300

    Delete AIVOZ directory

--- AIVOZ/js/script_save.js
+++
@@ -1,8 +0,0 @@
-function salvar(){
-    let codigo = editor.getValue();
-    let blob = new Blob([codigo],
-    {
-            type: "html/plain;charset=utf-8"
-    });
-    saveAs(blob, "index.html");
-}
